cf30da5ea7deda977a1e67e82750571d9c439b4a
Сабурлаев Денис <mr.saburlaev@mail.ru>
2022-08-13 10:08:38 +0300

    Проверка на работоспособность

--- impl/src/main/java/com/company/services/ProductService.java
+++ impl/src/main/java/com/company/services/ProductService.java
@@ -51,7 +51,7 @@ public class ProductService {
         return product1.getCipher();
     }
 
-    public void addProductList (MultipartFile multipartFile){
+    public void addProductList (MultipartFile multipartFile){//для проверки
         List<Product> list = ExcelParser.excelParsing(multipartFile);
         for (Product product : list){
             Optional<Product> oProduct1 = productRepository.findByCipher(product.getCipher());
