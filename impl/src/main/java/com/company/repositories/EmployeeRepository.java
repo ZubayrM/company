242a8c62417b8431977eb0f22b045784b05e85e4
saburlaev <mr.saburlaev@mail.ru>
2022-10-18 16:54:55 +0300

    Удалены все коммиты, связанные с аутентификацией. Попробую всё сделать заново

--- impl/src/main/java/com/company/repositories/EmployeeRepository.java
+++ impl/src/main/java/com/company/repositories/EmployeeRepository.java
@@ -4,9 +4,6 @@ import com.company.domain.models.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface EmployeeRepository extends JpaRepository <Employee, Long> {
-    Optional<Employee> findByUsername(String username);
 }
